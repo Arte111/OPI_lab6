af97ef81e88e864feab11e3426afce7dc1b40e11
Arte111 <artem.emelianov.2004@gmail.com>
2022-11-22 18:34:11 +0700

    v1.2 add trigonometry

--- model.py
+++ model.py
@@ -4,13 +4,17 @@ import numpy as np
 
 class Pmodel(object):
     def __init__(self, array_type="int", amount=None, minvalue=None, maxvalue=None):
-        match array_type:
-            case "int": self.array = np.random.randint(minvalue, maxvalue, size=(1, amount))
-            case "float": self.array = np.random.uniform(minvalue, maxvalue, size=(1, amount))
+        if array_type == "int":
+            self.array = np.random.randint(minvalue, maxvalue, size=(1, amount))
+        elif array_type == "float":
+            self.array = np.random.uniform(minvalue, maxvalue, size=(1, amount))
+
         self.time = time()
         self.work_time = 0
 
+    """ Functions for arithmetic """
     def smart_return(self, result):
+        # return result (number) or error and work time
         # match-case DON'T work with -np.inf here, I don't know why
         self.work_time = time() - self.time
         if result == -np.inf:
@@ -36,5 +40,23 @@ class Pmodel(object):
     def div_array(self):
         return self.smart_return(1 / np.prod(self.array))
 
+    """ Functions for trigonometry 
+        return all array and time """
+    def sin_array(self):
+        # return np.sin(self.array)[0].tolist(), time()-self.time
+        np.sin(self.array)
+        return time()-self.time
+
+    def cos_array(self):
+        # return np.cos(self.array)[0].tolist(), time()-self.time
+        np.cos(self.array)
+        return time() - self.time
+
+    def tan_array(self):
+        # return np.tan(self.array)[0].tolist(), time()-self.time
+        np.tan(self.array)
+        return time() - self.time
+
+    """ Function for all """
     def get_array(self):
         return self.__dict__["array"][0]
--- ui.py
+++ ui.py
@@ -3,9 +3,12 @@
 from controller import model
 
 b = model(array_type="int", amount=1000, minvalue=-100, maxvalue=100)
-a = model("float", 100000, 0, 1)
+a = model("float", 10000000, -2, 1)
 print(a.sum_array())
 print(a.diff_array())
 print(a.mult_array())
 print(a.div_array())
+print(a.sin_array())
+print(a.cos_array())
+print(a.tan_array())
 print(a.get_array())
