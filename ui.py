fe22b9dd699058b6b02f777a75c0f3b35e491926
Skublik <yarikskublik@outlook.com>
2022-11-22 17:47:45 +0300

    GUI version 1.0

--- ui.py
+++ ui.py
@@ -1,14 +1,49 @@
-# По идеи ui должен обращаться к controller, чтобы не было связей с model
-# такая штука называется MVC (model view controller)
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+import sys
+
 from controller import model
 
-b = model(array_type="int", amount=1000, minvalue=-100, maxvalue=100)
-a = model("float", 10000000, -2, 1)
-print(a.sum_array())
-print(a.diff_array())
-print(a.mult_array())
-print(a.div_array())
-print(a.sin_array())
-print(a.cos_array())
-print(a.tan_array())
-print(a.get_array())
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.createB.clicked.connect(self.create_func)
+        self.clear.clicked.connect(self.clear_func)
+
+    def create_func(self):
+        if self.min.value() < self.max.value():
+            massObj = model(array_type=self.typeVar.currentText(), 
+                            amount=int(self.many.value()), 
+                            minvalue=int(self.min.value()), 
+                            maxvalue=int(self.max.value()))
+            array = massObj.get_array()
+            self.mainMass.clear()
+            for elem in array:
+                self.mainMass.addItem(str(elem))
+            if self.typeAct.currentText() == "Сложение":
+                self.logs.addItem(f"Значение: {massObj.sum_array()[0]}\tВремя: {massObj.sum_array()[1]}")
+            if self.typeAct.currentText() == "Вычитание":
+                self.logs.addItem(f"Значение: {massObj.diff_array()[0]}\tВремя: {massObj.diff_array()[1]}")
+            if self.typeAct.currentText() == "Умножение":
+                self.logs.addItem(f"Значение: {massObj.mult_array()[0]}\tВремя: {massObj.mult_array()[1]}")
+            if self.typeAct.currentText() == "Деление":
+                self.logs.addItem(f"Значение: {massObj.div_array()[0]}\tВремя: {massObj.div_array()[1]}")
+            if self.typeAct.currentText() == "sin":
+                self.logs.addItem(f"Время: {massObj.sin_array()}")
+            if self.typeAct.currentText() == "cos":
+                self.logs.addItem(f"Время: {massObj.cos_array()}")
+            if self.typeAct.currentText() == "tan":
+                self.logs.addItem(f"Время: {massObj.tan_array()}")
+
+    def clear_func(self):
+        self.logs.clear()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
