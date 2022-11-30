0d6082768acecd8701e99ce803fb57d725f6a3d0
Arte111 <artem.emelianov.2004@gmail.com>
2022-11-30 21:11:11 +0700

    model for xlsx save

--- save.py
+++ save.py
@@ -1,2 +1,16 @@
-# здесь сохранение в эксель, но после того, как определимся с выходными данными
-# например, конфигурация пк, количество элементов, тип, диапазон, время работы, комментарий
+from openpyxl import load_workbook
+
+
+class Saver:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def add_on_top(self, data):
+        # append don't work here, I don't know why
+        work_book = load_workbook(self.filename)
+        sheet = work_book.active  # do active one page
+        sheet.insert_rows(1)  # add row on the top
+        for cols, value in enumerate(data):
+            sheet.cell(row=1, column=cols + 1).value = value
+
+        work_book.save(self.filename)
