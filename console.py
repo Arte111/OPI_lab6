30cc517477a4a85d4d70ee96822bac61d56cedf1
Arte111 <artem.emelianov.2004@gmail.com>
2022-11-30 21:15:37 +0700

    for fast analysis

--- console.py
+++ console.py
@@ -1,8 +1,13 @@
-from controller import model
+from controller import model, save
 
-amount = 300_000_000
-mi = 0.501
+epoch = 6
+amount = 100_000_000
+mi = 0.5084
 ma = 1.59
+t = "double"
 
-for i in [model('array', 'double', amount, mi, ma) for i in range(6)]:
-    print(i.mult_array())
+file = save("logs/mult_double.xlsx")
+
+for i in [model('array', t, amount, mi, ma) for i in range(epoch)]:
+    mult = i.mult_array()
+    file.add_on_top([mi, ma, mult[0], mult[1], mult[2]])
