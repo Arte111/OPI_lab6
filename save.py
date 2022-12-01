501eafb81ee58af692ad11f9dd21dcc91a3d18f9
Arte111 <artem.emelianov.2004@gmail.com>
2022-12-01 19:58:26 +0700

    better saver

--- save.py
+++ save.py
@@ -4,13 +4,13 @@ from openpyxl import load_workbook
 class Saver:
     def __init__(self, filename):
         self.filename = filename
+        self.work_book = load_workbook(self.filename)
+        self.sheet = self.work_book.active  # do active one page
 
     def add_on_top(self, data):
         # append don't work here, I don't know why
-        work_book = load_workbook(self.filename)
-        sheet = work_book.active  # do active one page
-        sheet.insert_rows(1)  # add row on the top
+        self.sheet.insert_rows(2)  # add row on the top
         for cols, value in enumerate(data):
-            sheet.cell(row=1, column=cols + 1).value = value
+            self.sheet.cell(row=2, column=cols + 1).value = value
 
-        work_book.save(self.filename)
+        self.work_book.save(self.filename)
