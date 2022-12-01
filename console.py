bfd358bfeba9791e06771ba97261cd573bb9e2f1
Arte111 <artem.emelianov.2004@gmail.com>
2022-12-01 15:05:52 +0700

    successively

--- console.py
+++ console.py
@@ -1,13 +1,14 @@
 from controller import model, save
 
-epoch = 6
-amount = 100_000_000
-mi = 0.5084
-ma = 1.59
-t = "double"
+epoch = 5
+amount = 575_000_000
+mi = -1000
+ma = 1000
+t = "int"
 
-file = save("logs/mult_double.xlsx")
+file = save("logs/int_sum.xlsx")
 
-for i in [model('array', t, amount, mi, ma) for i in range(epoch)]:
-    mult = i.mult_array()
-    file.add_on_top([mi, ma, mult[0], mult[1], mult[2]])
+for i in range(epoch):
+    res = model('array', t, amount, mi, ma).sum_array()
+    print(res)
+    file.add_on_top([mi, ma, amount, res[0], res[1], res[2]])
