03d3bb61618687b96765849895175e7e68cc26a9
Arte111 <artem.emelianov.2004@gmail.com>
2022-12-09 22:06:49 +0700

    optimization

--- DecimalModel.py
+++ DecimalModel.py
@@ -2,20 +2,23 @@ from decimal import *
 import random as rd
 from time import time
 from pprint import pprint
+from math import prod
+from joblib import Parallel, delayed
 
 prec = 500
 getcontext().prec = prec
 
 
-def dec_random(a, b):
+def dec_random(minimal, maximum):
     s = str('0.' + ''.join(rd.choice('0123456789') for _ in range(prec)))
-    return Decimal(a) + (Decimal(b) - Decimal(a)) * Decimal(s)
+    return Decimal(minimal) + (Decimal(maximum) - Decimal(minimal)) * Decimal(s)
 
 
 class DecimalModel:
     def __init__(self, array_type="dec", amount=None, minvalue=None, maxvalue=None):
         self.arr_time = time()
-        self.array = [dec_random(minvalue, maxvalue) for i in range(amount)]
+        # self.array = [dec_random(minvalue, maxvalue) for _ in range(amount)]
+        self.array = Parallel(n_jobs=-1)(delayed(dec_random)(minvalue, maxvalue) for _ in range(amount))
         self.arr_time = time() - self.arr_time
         self.work_time = 0
 
@@ -27,6 +30,23 @@ class DecimalModel:
         self.work_time = time()
         return self.smart_return(sum(self.array))
 
+    def diff_array(self):
+        self.work_time = time()
+        return self.smart_return(-1 * sum(self.array))
 
-a = DecimalModel('dec', 1_000_000_000, 0, 1)
+    def mult_array(self):
+        self.work_time = time()
+        return self.smart_return(prod(self.array))
+
+    def get_array(self):
+        return self.__dict__["array"]
+
+    def get_min(self):
+        return min(self.array)
+
+    def get_max(self):
+        return max(self.array)
+
+
+a = DecimalModel('dec', 1_000_000, 10, 100)
 pprint(a.sum_array())
