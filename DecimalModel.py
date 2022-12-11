be3d28dec5739eb8fb38219bff655e0402957f47
Arte111 <artem.emelianov.2004@gmail.com>
2022-12-11 16:25:43 +0700

    workable Decimal

--- DecimalModel.py
+++ DecimalModel.py
@@ -1,27 +1,39 @@
+from joblib import Parallel, delayed
+from multiprocessing import Pool
 from decimal import *
+from math import prod
 import random as rd
 from time import time
 from pprint import pprint
-from math import prod
-from joblib import Parallel, delayed
+import sys
 
 prec = 500
 getcontext().prec = prec
+
+'''f = open('choice_data.txt', 'r')
+
+choice_data = [line.strip() for line in f]'''
 
 choice_data = [i1 + i2 + i3 + i4 for i1 in '0123456789' for i2 in '0123456789' \
                for i3 in '0123456789' for i4 in '0123456789']
 
 
-def dec_random(minimal, maximum):
-    s = str('0.' + ''.join(rd.choice(choice_data) for _ in range(prec // 4)))
-    return Decimal(minimal) + (Decimal(maximum) - Decimal(minimal)) * Decimal(s)
+# print(sys.getsizeof(dec_random(10, 100)))
+
+
+def dec_random(data):
+    # data = [minvalue, maxvalue]
+    s = str('0.' + ''.join(rd.choice(choice_data) for _ in range(prec//4)))
+    return Decimal(data[0]) + (Decimal(data[1]) - Decimal(data[0])) * Decimal(s)
 
 
 class DecimalModel:
+
     def __init__(self, array_type="dec", amount=None, minvalue=None, maxvalue=None):
         self.arr_time = time()
         # self.array = [dec_random(minvalue, maxvalue) for _ in range(amount)]
-        self.array = Parallel(n_jobs=-1)(delayed(dec_random)(minvalue, maxvalue) for _ in range(amount))
+        # Parallel(n_jobs=-1)(delayed(dec_random)(minvalue, maxvalue) for _ in range(amount))
+        self.array = Pool().map(dec_random, [(minvalue, maxvalue) for _ in range(amount)])
         self.arr_time = time() - self.arr_time
         self.work_time = 0
 
@@ -51,5 +63,6 @@ class DecimalModel:
         return max(self.array)
 
 
-a = DecimalModel('dec', 1_000_000, 10, 100)
-pprint(a.sum_array())
+if __name__ == '__main__':
+    a = DecimalModel('dec', 1_000_000, 10, 100)
+    pprint(a.sum_array())
--- controller.py
+++ controller.py
@@ -1,5 +1,7 @@
-from NumPyModel import NumPyModel
+# from NumPyModel import NumPyModel
+from DecimalModel import DecimalModel
 from save import Saver
 
-model = NumPyModel
+# model = NumPyModel
+model = DecimalModel
 save = Saver  # Saver(path to file).add_on_top(massive)
